386e253a67d659e9fcf049049bdf050fb13d6371
shubhamgupta80 <shabugupta80@gmail.com>
2024-04-13 23:06:13 +0530

    new

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Category, Contact, Dish
+from .models import Category, Contact, Dish, Product, Testimonial,Gallery
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Contact)
 admin.site.register(Dish)
+admin.site.register(Product)
+admin.site.register(Testimonial)
+admin.site.register(Gallery)
--- shop/models.py
+++ shop/models.py
@@ -38,3 +38,39 @@ class Dish(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='Product')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    available = models.BooleanField(default=True)
+    best_seller = models.BooleanField(default=False)
+    seller = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+
+    def __str__(self):
+        return self.title
+    
+class Testimonial(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='Product')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+
+    def __str__(self):
+        return self.title
+    
+class Gallery(models.Model):
+    image = models.ImageField(upload_to='Product')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+
+    def __str__(self):
+        return self.created_at
