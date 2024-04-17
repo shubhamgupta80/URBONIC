6b1a857c8ea345fee0e7019c15cc650a66db1142
shubhamgupta80 <shabugupta80@gmail.com>
2024-04-17 14:14:37 +0530

    new change

--- shop/admin.py
+++ shop/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import Category, Contact, Dish, Product, Testimonial,Gallery
+from .models import Category, Contact, Product, Testimonial,Gallery
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Contact)
-admin.site.register(Dish)
 admin.site.register(Product)
 admin.site.register(Testimonial)
 admin.site.register(Gallery)
