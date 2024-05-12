9b40a954b23ba183fd27a57c0cc31f2d7e6628c1
shubhamgupta80 <shabugupta80@gmail.com>
2024-05-12 18:45:25 +0530

    payment

--- shop/views.py
+++ shop/views.py
@@ -7,6 +7,12 @@ from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, Group
 from django.contrib.auth import authenticate, login, logout
+import dotenv
+import stripe
+import os
+from django.conf import settings
+import json
+from django.core.mail import send_mail
 
 def home_view(request):
     articles = Article.objects.all()[:3]
